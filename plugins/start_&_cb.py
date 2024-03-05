cba7fae8723203f11327362cbd03888f221f96ef
rahul torat <lorena203232@proton.me>
2024-03-06 02:25:32 +0530

    Update start_&_cb.py

--- plugins/start_&_cb.py
+++ plugins/start_&_cb.py
@@ -11,11 +11,8 @@ async def start(client, message):
     user = message.from_user
     await jishubotz.add_user(client, message)                
     button = InlineKeyboardMarkup([
-        [InlineKeyboardButton('🔊 Updates', url='https://t.me/Madflix_Bots'),
-        InlineKeyboardButton('♻️ Sᴜᴩᴩᴏʀᴛ', url='https://t.me/MadflixBots_Support')],
-        [InlineKeyboardButton('❤️‍🩹 About', callback_data='about'),
-        InlineKeyboardButton('🛠️ Help', callback_data='help')],
-        [InlineKeyboardButton("👨‍💻 Developer", url='https://t.me/CallAdminRobot')]
+        [InlineKeyboardButton('⚔ About', callback_data='about'),
+        InlineKeyboardButton('⚙ Help', callback_data='help')]
     ])
     if Config.START_PIC:
         await message.reply_photo(Config.START_PIC, caption=Txt.START_TXT.format(user.mention), reply_markup=button)       
@@ -31,19 +28,15 @@ async def cb_handler(client, query: CallbackQuery):
             text=Txt.START_TXT.format(query.from_user.mention),
             disable_web_page_preview=True,
             reply_markup = InlineKeyboardMarkup([
-                [InlineKeyboardButton('🔊 Updates', url='https://t.me/Madflix_Bots'),
-                InlineKeyboardButton('♻️ Sᴜᴩᴩᴏʀᴛ', url='https://t.me/MadflixBots_Support')],
-                [InlineKeyboardButton('❤️‍🩹 About', callback_data='about'),
-                InlineKeyboardButton('🛠️ Help', callback_data='help')],
-                [InlineKeyboardButton("👨‍💻 Developer", url='https://t.me/CallAdminRobot')]
-            ])
+        [InlineKeyboardButton('⚔ About', callback_data='about'),
+        InlineKeyboardButton('⚙ Help', callback_data='help')]
+    ])
         )
     elif data == "help":
         await query.message.edit_text(
             text=Txt.HELP_TXT,
             disable_web_page_preview=True,
-            reply_markup=InlineKeyboardMarkup([
-                [InlineKeyboardButton("⚡ 4GB Rename Bot", url="https://t.me/FileRenameXProBot")],
+            reply_markup=InlineKeyboardMarkup(
                 [InlineKeyboardButton("🔒 Close", callback_data = "close"),
                 InlineKeyboardButton("◀️ Back", callback_data = "start")]
             ])            
@@ -52,8 +45,7 @@ async def cb_handler(client, query: CallbackQuery):
         await query.message.edit_text(
             text=Txt.ABOUT_TXT.format(client.mention),
             disable_web_page_preview = True,
-            reply_markup=InlineKeyboardMarkup([
-                [InlineKeyboardButton("🤖 More Bots", url="https://t.me/Madflix_Bots/7")],
+            reply_markup=InlineKeyboardMarkup(
                 [InlineKeyboardButton("🔒 Cʟᴏꜱᴇ", callback_data = "close"),
                 InlineKeyboardButton("◀️ Bᴀᴄᴋ", callback_data = "start")]
             ])            
@@ -66,18 +58,6 @@ async def cb_handler(client, query: CallbackQuery):
         except:
             await query.message.delete()
             await query.message.continue_propagation()
-
-
-
-
-
-@Client.on_message(filters.private & filters.command(["donate", "d"]))
-async def donate(client, message):
-	text = Txt.DONATE_TXT
-	keybord = InlineKeyboardMarkup([
-        			[InlineKeyboardButton("🦋 Admin",url = "https://t.me/CallAdminRobot"), 
-        			InlineKeyboardButton("✖️ Close",callback_data = "close") ]])
-	await message.reply_text(text = text,reply_markup = keybord)
 
 
 
