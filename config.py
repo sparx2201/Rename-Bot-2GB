1fe5e52ec3f07f8ccd5d0f0ece0f35731c5749ea
rahul torat <lorena203232@proton.me>
2024-03-06 03:02:05 +0530

    Update config.py

--- config.py
+++ config.py
@@ -35,41 +35,42 @@ class Txt(object):
 ➻ Yᴏᴜ Cᴀɴ Aʟꜱᴏ Cᴏɴᴠᴇʀᴛ Vɪᴅᴇᴏ Tᴏ Fɪʟᴇ Aɴᴅ Fɪʟᴇ Tᴏ Vɪᴅᴇᴏ.📁»🎬
 
 ➻ Tʜɪꜱ Bᴏᴛ Aʟꜱᴏ Sᴜᴘᴘᴏʀᴛꜱ Cᴜꜱᴛᴏᴍ Tʜᴜᴍʙɴᴀɪʟ Aɴᴅ Cᴜꜱᴛᴏᴍ Cᴀᴘᴛɪᴏɴ.⚙️
+
 </b>"""
 
     
     ABOUT_TXT = """
 ╭───────────────⍟
-├<b>My Name</b> : 𝐂𝐨𝐝𝐞-𝐀𝟏𝟒-𝐑
-├<b>Created by : 𝘈𝘑
-├<b>Library</b> : <a href=https://github.com/pyrogram>Pyrogram</a>
-├<b>Language</b> : <a href=https://www.python.org>Python 3</a>
-├<b>Database</b> : <a href=https://cloud.mongodb.com>Mongo DB</a>
-├<b>Build Version</b> : <a href=https://instagram.com/jishu.editz>Rename v4.5.0</a></b>     
+├<b> My Name</b> : 𝐂𝐨𝐝𝐞-𝐀𝟏𝟒-𝐑
+├<b> Created by : 𝘈𝘑
+├<b> Library</b> : <a href=https://github.com/pyrogram>Pyrogram</a>
+├<b> Language</b> : <a href=https://www.python.org>Python 3</a>
+├<b> Database</b> : <a href=https://cloud.mongodb.com>Mongo DB</a>
+├<b> Build Version</b> : <a href=https://instagram.com/jishu.editz>Rename v4.5.0</a></b>     
 ╰───────────────⍟
 """
 
     HELP_TXT = """
-🌌 <b><u>How To Set Thumbnail</u></b>
+🖼 <b><u>How To Set Thumbnail</u></b>
   
-➪ /start - Start The Bot And Send Any Photo To Automatically Set Thumbnail.
-➪ /del_thumb - Use This Command To Delete Your Old Thumbnail.
-➪ /view_thumb - Use This Command To View Your Current Thumbnail.
+▸ /start - Start The Bot And Send Any Photo To Automatically Set Thumbnail.
+▸ /del_thumb - Use This Command To Delete Your Old Thumbnail.
+▸ /view_thumb - Use This Command To View Your Current Thumbnail.
 
-📑 <b><u>How To Set Custom Caption</u></b>
+📜 <b><u>How To Set Custom Caption</u></b>
 
-➪ /set_caption - Use This Command To Set A Custom Caption
-➪ /see_caption - Use This Command To View Your Custom Caption
-➪ /del_caption - Use This Command To Delete Your Custom Caption
-➪ Example - <code>/set_caption 📕 Name ➠ : {filename}
+▸ /set_caption - Use This Command To Set A Custom Caption
+▸ /see_caption - Use This Command To View Your Custom Caption
+▸ /del_caption - Use This Command To Delete Your Custom Caption
+▸ Example - <code>/set_caption 📕 Name ➠ : {filename}
 
 🔗 Size ➠ : {filesize} 
 
-⏰ Duration ➠ : {duration}</code>
+🕰 Duration ➠ : {duration}</code>
 
 ✏️ <b><u>How To Rename A File</u></b>
 
-➪ Send Any File And Type New File Name And Select The Format [ Document, Video, Audio ].
+▸ Send Any File And Type New File Name And Select The Format [ Document, Video, Audio ].
 """
 
 
